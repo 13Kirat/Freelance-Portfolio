e6d3c084054d16eca00a5b425265e63cd037fafd
13Kirat <141816179+13Kirat@users.noreply.github.com>
2025-11-08 06:10:26 +0530

    ...

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,16 +4,16 @@ import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.cookies;
-  console.log(token)
-  if (!token) {
-    req.user = await User.findById("69076682c3404c301e494b6d");
-    next();
-    // return next(new ErrorHandler("User not Authenticated!", 400));
-  }
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-  console.log(decoded)
-  req.user = await User.findById(decoded.id);
-  console.log(req.user)
+  // const { token } = req.cookies;
+  // console.log(token)
+  // if (!token) {
+  req.user = await User.findById("69076682c3404c301e494b6d");
+  // next();
+  // return next(new ErrorHandler("User not Authenticated!", 400));
+  // }
+  // const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  // console.log(decoded)
+  // req.user = await User.findById(decoded.id);
+  // console.log(req.user)
   next();
 });
